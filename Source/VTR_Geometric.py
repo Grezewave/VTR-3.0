8ed9e5d7291b9960c37df70e542eb74bb2b87474
Grezewave <55059123+Grezewave@users.noreply.github.com>
2021-02-25 18:20:01 -0300

    bug fix

--- Source/VTR_Geometric.py
+++ Source/VTR_Geometric.py
@@ -97,11 +97,11 @@ class Match:
             self.rtt_protein = rtt_protein
             self.stc_protein = stc_protein
             if 'd' in _type:
-                rtt_contacts = Contacts.contacts(rtt_protein,protein1[protein1.rfind("/"):-4] + "_rotate",_chain11,_chain12,'d')
-                stc_contacts = Contacts.contacts(stc_protein,protein2[protein2.rfind("/"):-4],_chain21,_chain22,'d')
+                rtt_contacts = Contacts.contacts(rtt_protein,protein1[protein1.rfind("/")+1:-4] + "_rotate",_chain11,_chain12,'d')
+                stc_contacts = Contacts.contacts(stc_protein,protein2[protein2.rfind("/")+1:-4],_chain21,_chain22,'d')
             else:
-                rtt_contacts = Contacts.contacts(rtt_protein,protein1[protein1.rfind("/"):-4] + "_rotate",_chain11,_chain12)
-                stc_contacts = Contacts.contacts(stc_protein,protein2[protein2.rfind("/"):-4],_chain21,_chain22)
+                rtt_contacts = Contacts.contacts(rtt_protein,protein1[protein1.rfind("/")+1:-4] + "_rotate",_chain11,_chain12)
+                stc_contacts = Contacts.contacts(stc_protein,protein2[protein2.rfind("/")+1:-4],_chain21,_chain22)
 
             self.rtt_contacts = rtt_contacts
             self.stc_contacts = stc_contacts
@@ -217,34 +217,34 @@ class Match:
         except:
             traceback.print_exc(file=open('error_logs.txt','w'))
 
-    def to_csv(self,_list, outname):
-        out = open(outname, 'w')
-        stack = ''''''
-        for i in _list:
-            if type(i) is Contacts.Contact:
-                stack+=i.chain1.id + ',' + i.residue1.id + str(i.residue1.parameter) + ',' + i.atom1.type.strip() + ','
-                stack+=i.chain2.id + ',' + i.residue2.id + str(i.residue2.parameter) + ',' + i.atom2.type.strip() + ',' 
-                stack+=str(round(i.distance,2)) + ','
-                for x in i.contacts:
-                    stack += x + ' '
-                stack += '''
+def to_csv(_list, outname):
+    out = open(outname, 'w')
+    stack = ''''''
+    for i in _list:
+        if type(i) is Contacts.Contact:
+            stack+=i.chain1.id + ',' + i.residue1.id + str(i.residue1.parameter) + ',' + i.atom1.type.strip() + ','
+            stack+=i.chain2.id + ',' + i.residue2.id + str(i.residue2.parameter) + ',' + i.atom2.type.strip() + ',' 
+            stack+=str(round(i.distance,2)) + ','
+            for x in i.contacts:
+                stack += x + ' '
+            stack += '''
 '''
-            elif type(i) is vtr.match:
-                stack+=i.rtt_contact.chain1.id + ',' + i.rtt_contact.residue1.id + str(i.rtt_contact.residue1.parameter) + ',' + i.rtt_contact.atom1.type.strip() + ','
-                stack+=i.rtt_contact.chain2.id + ',' + i.rtt_contact.residue2.id + str(i.rtt_contact.residue2.parameter) + ',' + i.rtt_contact.atom2.type.strip() + ',' 
-                stack+=str(round(i.rtt_contact.distance,2)) + ','
-                for x in i.rtt_contact.contacts:
-                    stack += x + ' '
-                stack += ','
-                stack+=i.stc_contact.chain1.id + ',' + i.stc_contact.residue1.id + str(i.stc_contact.residue1.parameter) + ',' + i.stc_contact.atom1.type.strip() + ','
-                stack+=i.stc_contact.chain2.id + ',' + i.stc_contact.residue2.id + str(i.stc_contact.residue2.parameter) + ',' + i.stc_contact.atom2.type.strip() + ',' 
-                stack+=str(round(i.stc_contact.distance,2)) + ','
-                for x in i.stc_contact.contacts:
-                    stack += x + ' '
-                stack += ','
-                stack += str(round(i.VMD(),2)) + '''
+        elif type(i) is vtr.match:
+            stack+=i.rtt_contact.chain1.id + ',' + i.rtt_contact.residue1.id + str(i.rtt_contact.residue1.parameter) + ',' + i.rtt_contact.atom1.type.strip() + ','
+            stack+=i.rtt_contact.chain2.id + ',' + i.rtt_contact.residue2.id + str(i.rtt_contact.residue2.parameter) + ',' + i.rtt_contact.atom2.type.strip() + ',' 
+            stack+=str(round(i.rtt_contact.distance,2)) + ','
+            for x in i.rtt_contact.contacts:
+                stack += x + ' '
+            stack += ','
+            stack+=i.stc_contact.chain1.id + ',' + i.stc_contact.residue1.id + str(i.stc_contact.residue1.parameter) + ',' + i.stc_contact.atom1.type.strip() + ','
+            stack+=i.stc_contact.chain2.id + ',' + i.stc_contact.residue2.id + str(i.stc_contact.residue2.parameter) + ',' + i.stc_contact.atom2.type.strip() + ',' 
+            stack+=str(round(i.stc_contact.distance,2)) + ','
+            for x in i.stc_contact.contacts:
+                stack += x + ' '
+            stack += ','
+            stack += str(round(i.VMD(),2)) + '''
 '''
-            else:
-                print('Incorrect input(Must be a list of Contacts or vtr.matches)')
-                return False
-        out.write(stack)
+        else:
+            print('Incorrect input(Must be a list of Contacts or vtr.matches)')
+            return False
+    out.write(stack)
